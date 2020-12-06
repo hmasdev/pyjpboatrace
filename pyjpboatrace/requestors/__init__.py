0f7d586e26f40c2ccc2f5874114ba4e65d403719
hmasdev <hmasdev@gmail.com>
2020-12-06 20:25:59 +0900

    add requestor with login

--- pyjpboatrace/requestors/__init__.py
+++ pyjpboatrace/requestors/__init__.py
@@ -1,7 +1,9 @@
 from .base_requestor import BaseRequestor
 from .requestor import Requestor
+from .boatracejp_requestor import BoatracejpRequestor
 
 __all__ = [
     'BaseRequestor',
     'Requestor',
+    'BoatracejpRequestor',
 ]
