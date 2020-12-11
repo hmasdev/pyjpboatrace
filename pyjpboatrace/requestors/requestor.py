e3367a3005db4feeb48c3c906c3de2306e4fe01e
hmasdev <hmasdev@gmail.com>
2020-12-11 19:34:17 +0900

    add post to requestor

--- pyjpboatrace/requestors/requestor.py
+++ pyjpboatrace/requestors/requestor.py
@@ -18,3 +18,10 @@ class Requestor(BaseRequestor):
         http_response = requests.get(url)
         self.previous_called = time.time()
         return http_response
+
+    # TODO add test
+    def post(self, url: str, data: dict = {}) -> Response:
+        time.sleep(max(0, 1-time.time()+self.previous_called))
+        http_response = requests.post(url, data=data)
+        self.previous_called = time.time()
+        return http_response
