ed480a026269eff124180480d1b14003fe132ba3
hmasdev <hmasdev@gmail.com>
2020-12-28 17:20:44 +0900

    update __init__.py

--- pyjpboatrace/__init__.py
+++ pyjpboatrace/__init__.py
@@ -2,9 +2,13 @@ from .pyjpboatrace import PyJPBoatrace
 from . import exceptions
 from . import parsers
 from . import utils
+from . import actions
+from . import const
+from . import drivers
+from . import user_information
 
 __copyright__ = 'Copyright (C) 2020 hmasdev'
-__version__ = '0.0.0'
+__version__ = '0.1.0'
 __license__ = 'MIT'
 __author__ = 'hmasdev'
 __author_email__ = 'hmasuidev1com@gmail.com'
@@ -13,7 +17,11 @@ __url__ = 'http://github.com/hmasdev/pyjpboatrace'
 __all__ = [
     'PyJPBoatrace',
     'pyjpboatrace',
+    'actions',
     'parsers',
     'utils',
-    'exceptions'
+    'drivers',
+    'user_information',
+    'const',
+    'exceptions',
 ]
