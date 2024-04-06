fe72f25c25a3067726a183b9d40f33fd1b4d7e6a
hmasdev <hmasuidev@gmail.com>
2024-04-06 18:05:13 +0900

    fix indent

--- update_readme.py
+++ update_readme.py
@@ -5,46 +5,51 @@ from pyjpboatrace import PyJPBoatrace
 
 readme = Environment(loader=FileSystemLoader('.')).get_template('README.md.j2')
 
+
+def format_example_output(output_dict: dict) -> str:
+    return pprint.pformat(output_dict).replace('\n', '\n    ')
+
+
 examples = {
     'get_stadiums': {
-        'command': 'PyJPBoatrace().get_stadiums(date(2021, 8, 12))',
-        'output': pprint.pformat(PyJPBoatrace().get_stadiums(date(2021, 8, 12)), indent=4),
+        'command': 'PyJPBoatrace().get_stadiums(date(2021, 8, 12))',  # noqa
+        'output': format_example_output(PyJPBoatrace().get_stadiums(date(2021, 8, 12))),  # noqa
     },
     'get_12races': {
-        'command': 'PyJPBoatrace().get_12races(date(2021, 8, 12), 1)',
-        'output': pprint.pformat(PyJPBoatrace().get_12races(date(2021, 8, 12), 1), indent=4),
+        'command': 'PyJPBoatrace().get_12races(date(2021, 8, 12), 1)',  # noqa
+        'output': format_example_output(PyJPBoatrace().get_12races(date(2021, 8, 12), 1)),  # noqa
     },
     'get_race_info': {
-        'command': 'PyJPBoatrace().get_race_info(date(2021, 8, 12), 10, 1)',
-        'output': pprint.pformat(PyJPBoatrace().get_race_info(date(2021, 8, 12), 10, 1), indent=4),
+        'command': 'PyJPBoatrace().get_race_info(date(2021, 8, 12), 10, 1)',  # noqa
+        'output': format_example_output(PyJPBoatrace().get_race_info(date(2021, 8, 12), 10, 1)),  # noqa
     },
     'get_odds_win_placeshow': {
-        'command': 'PyJPBoatrace().get_odds_win_placeshow(date(2021, 8, 12), 10, 1)',
-        'output': pprint.pformat(PyJPBoatrace().get_odds_win_placeshow(date(2021, 8, 12), 10, 1), indent=4),
+        'command': 'PyJPBoatrace().get_odds_win_placeshow(date(2021, 8, 12), 10, 1)',  # noqa
+        'output': format_example_output(PyJPBoatrace().get_odds_win_placeshow(date(2021, 8, 12), 10, 1)),  # noqa
     },
     'get_odds_quinellaplace': {
-        'command': 'PyJPBoatrace().get_odds_quinellaplace(date(2021, 8, 12), 10, 1)',
-        'output': pprint.pformat(PyJPBoatrace().get_odds_quinellaplace(date(2021, 8, 12), 10, 1), indent=4),
+        'command': 'PyJPBoatrace().get_odds_quinellaplace(date(2021, 8, 12), 10, 1)',  # noqa
+        'output': format_example_output(PyJPBoatrace().get_odds_quinellaplace(date(2021, 8, 12), 10, 1)),  # noqa
     },
     'get_odds_exacta_quinella': {
-        'command': 'PyJPBoatrace().get_odds_exacta_quinella(date(2021, 8, 12), 10, 1)',
-        'output': pprint.pformat(PyJPBoatrace().get_odds_exacta_quinella(date(2021, 8, 12), 10, 1), indent=4),
+        'command': 'PyJPBoatrace().get_odds_exacta_quinella(date(2021, 8, 12), 10, 1)',  # noqa
+        'output': format_example_output(PyJPBoatrace().get_odds_exacta_quinella(date(2021, 8, 12), 10, 1)),  # noqa
     },
     'get_odds_trifecta': {
-        'command': 'PyJPBoatrace().get_odds_trifecta(date(2021, 8, 12), 10, 1)',
-        'output': pprint.pformat(PyJPBoatrace().get_odds_trifecta(date(2021, 8, 12), 10, 1), indent=4),
+        'command': 'PyJPBoatrace().get_odds_trifecta(date(2021, 8, 12), 10, 1)',  # noqa
+        'output': format_example_output(PyJPBoatrace().get_odds_trifecta(date(2021, 8, 12), 10, 1)),  # noqa
     },
     'get_odds_trio': {
-        'command': 'PyJPBoatrace().get_odds_trio(date(2021, 8, 12), 10, 1)',
-        'output': pprint.pformat(PyJPBoatrace().get_odds_trio(date(2021, 8, 12), 10, 1), indent=4),
+        'command': 'PyJPBoatrace().get_odds_trio(date(2021, 8, 12), 10, 1)',  # noqa
+        'output': format_example_output(PyJPBoatrace().get_odds_trio(date(2021, 8, 12), 10, 1)),  # noqa
     },
     'get_just_before_info': {
-        'command': 'PyJPBoatrace().get_just_before_info(date(2021, 8, 12), 10, 1)',
-        'output': pprint.pformat(PyJPBoatrace().get_just_before_info(date(2021, 8, 12), 10, 1), indent=4),
+        'command': 'PyJPBoatrace().get_just_before_info(date(2021, 8, 12), 10, 1)',  # noqa
+        'output': format_example_output(PyJPBoatrace().get_just_before_info(date(2021, 8, 12), 10, 1)),  # noqa
     },
     'get_race_result': {
-        'command': 'PyJPBoatrace().get_race_result(date(2021, 8, 12), 10, 1)',
-        'output': pprint.pformat(PyJPBoatrace().get_race_result(date(2021, 8, 12), 10, 1), indent=4),
+        'command': 'PyJPBoatrace().get_race_result(date(2021, 8, 12), 10, 1)',  # noqa
+        'output': format_example_output(PyJPBoatrace().get_race_result(date(2021, 8, 12), 10, 1)),  # noqa
     },
 }
 
